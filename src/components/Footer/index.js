9ba028b5cb4da6d27df343bd416bb020133d800a
sad-py <sabrina.dd@gmail.com>
2020-07-30 16:08:17 -0300

    bd_v2

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png'
 
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="./">
+        <img className="Logo" src={Logo} alt="Logo Badflix" />
       </a>
       <p>
-        Orgulhosamente criado durante a
-        {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
-        </a>
+        Dolorosamente criado durante o tédio.
       </p>
     </FooterBase>
   );
